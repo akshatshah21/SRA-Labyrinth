4eca593d29554fbfffcc83621110be1eeeb613b9
akshatshah21 <akshatshah_21@yahoo.in>
2019-08-09 10:03:42 +0530

    Started mapping code

--- src/mapping/mapping.c
+++ src/mapping/mapping.c
@@ -8,7 +8,7 @@
 int point[MAX_VERTICES]; // Array for storing every point, referred by the number in this array
 int type[MAX_VERTICES];  // Array for storing the type of corresponding point in point array
 int explored[MAX_VERTICES] = {0};  // Array for storing how many times the bot has explored corresponding point
-                    // in the point array
+                  				   // in the point array
 
 int coordinates[MAX_VERTICES][2];    //Storing coordinates of corresponding points in point array
 int point_count = 0;   // Number of distinct points encountered
@@ -22,5 +22,21 @@ enum direction dir = north;
 
 
 void map(void *arg) {
-    
+    point_count++;	//Start point
+	point[point_count - 1] = 0;	// Adding start to point list
+	explored[point_count-1] = 1;
+	type[point_count-1] = 0;
+	dir = north;	//Start direction
+	coordinates[point_count-1][0] = 0;
+	coordinates[point_count-1][1] = 0;
+
+	while(junction() == -1) {
+		printf("No junction\n");
+
+	}
+
+	type[point_count] = junction();
+
+
+
 }
