52624fa2f6a72ade976ae6f0154a75874acb1b90
akshatshah21 <akshatshah_21@yahoo.in>
2019-08-12 19:39:46 +0530

    LS test

--- src/LSTest/main/LSTest.c
+++ src/LSTest/main/LSTest.c
@@ -10,33 +10,72 @@
 #include "SRA18.h"
 #include "TUNING.h"
 
-#define LS 32
+#define LS_LEFT 25
+#define LS_RIGHT 26
+// #define LS_AHEAD 32
 
-void blink_task(void *arg)
+void blink_task1(void *arg)
 {
 	/*
 		Set the The LED Pins : GPIO 0 and GPIO 5 to OUTPUT
 	*/
 
-    gpio_set_direction(LS,GPIO_MODE_INPUT);
+    gpio_set_direction(LS_LEFT,GPIO_MODE_INPUT);
+	// gpio_set_direction(LS_RIGHT,GPIO_MODE_INPUT);
+	// gpio_set_direction(LS,GPIO_MODE_INPUT);
 	gpio_set_direction(LED_1,GPIO_MODE_OUTPUT);
-	gpio_set_direction(LED_2,GPIO_MODE_OUTPUT);	
+	// gpio_set_direction(LED_2,GPIO_MODE_OUTPUT);
+	
 
 	while(1)
 	{
-        if(gpio_get_level(LS) == 1)
+        if(gpio_get_level(LS_LEFT) == 1)
         {
 		gpio_set_level(LED_1,0);	//Set LED1 ON
-		gpio_set_level(LED_2,0);	//Set LED2 ON
-        printf("WHITE\n");
-        vTaskDelay(500/portTICK_PERIOD_MS);
+		// gpio_set_level(LED_2,0);	//Set LED2 ON
+        printf("LS LEFT:\tBLACK\n");
+        vTaskDelay(50/portTICK_PERIOD_MS);
         }
         else
         {
 		gpio_set_level(LED_1,1);	//Set LED1 OFF
+		// gpio_set_level(LED_2,1);	//Set LED2 OFF
+        printf("LS LEFT:\tWHITE\n");
+        vTaskDelay(50/portTICK_PERIOD_MS);
+        }
+	}
+	
+}
+
+
+void blink_task2(void *arg)
+{
+	/*
+		Set the The LED Pins : GPIO 0 and GPIO 5 to OUTPUT
+	*/
+
+    // gpio_set_direction(LS_LEFT,GPIO_MODE_INPUT);
+	gpio_set_direction(LS_RIGHT,GPIO_MODE_INPUT);
+	// gpio_set_direction(LS,GPIO_MODE_INPUT);
+	// gpio_set_direction(LED_1,GPIO_MODE_OUTPUT);
+	gpio_set_direction(LED_2,GPIO_MODE_OUTPUT);
+	
+
+	while(1)
+	{
+        if(gpio_get_level(LS_RIGHT) == 1)
+        {
+		// gpio_set_level(LED_1,0);	//Set LED1 ON
+		gpio_set_level(LED_2,0);	//Set LED2 ON
+        printf("LS RIGHT:\tBLACK\n");
+        vTaskDelay(50/portTICK_PERIOD_MS);
+        }
+        else
+        {
+		// gpio_set_level(LED_1,1);	//Set LED1 OFF
 		gpio_set_level(LED_2,1);	//Set LED2 OFF
-        printf("BLACK\n");
-        vTaskDelay(500/portTICK_PERIOD_MS);
+        printf("LS RIGHT:\tWHITE\n");
+        vTaskDelay(50/portTICK_PERIOD_MS);
         }
 	}
 	
@@ -48,5 +87,6 @@ void app_main()
 		Basic Function for task creation
 	*/
 
-    xTaskCreate(&blink_task,"blink task",4096,NULL,1,NULL);
+    xTaskCreate(&blink_task1,"blink task1",4096,NULL,1,NULL);
+	xTaskCreate(&blink_task2, "blink_task2", 4096, NULL, 1, NULL);
 }
--- src/distance_counter/main/encoder.c
+++ src/distance_counter/main/encoder.c
@@ -13,13 +13,56 @@
 #define left 17
 #define right 18
 
+int counter_left = 0;
+int counter_right = 0;
+
+
+
+void countToDistance()
+{
+    double count_average,distance_final;
+    count_average = (counter_left + counter_right)/2;
+    printf("Average value of counter %g",count_average);
+    distance_final = (count_average * 20.3)/20;
+    printf("Final distance is: %g",distance_final);
+
+}
+
+void http_netconn_serve(struct netconn *conn) {
+    const static char http_html_hdr[] = "HTTP/1.1 200 OK\r\nContent-type: text/html\r\n\r\n";
+    const static char http_index_hml[] = "<html><head><title>Encoder distance</title></head><body></body></html>";
+    char dist[5];   // char array for storing distance
+
+    netconn_write(conn, http_html_hdr, sizeof(http_html_hdr)-1, NETCONN_NOCOPY);
+    netconn_write(conn, http_index_hml, sizeof(http_index_hml)-1, NETCONN_NOCOPY);
+    netconn_write(conn, &counter_left, 4, NETCONN_NOCOPY);
+    netconn_close(conn);
+}
+
+void http_server()
+{
+    struct netconn *conn, *newconn;
+    err_t err;
+    conn = netconn_new(NETCONN_TCP);
+    netconn_bind(conn, NULL, 80);
+    netconn_listen(conn);
+    do {
+        err = netconn_accept(conn, &newconn);   
+        if(err == ERR_OK) {
+            http_netconn_serve(newconn); //Serve distance
+            netconn_delete(newconn);
+        }
+    } while(err == ERR_OK);
+    netconn_close(conn);
+    netconn_delete(conn);
+}
+
 void distance_count(void *arg)
 {
 	/*
 		Set the The LED Pins : GPIO 0 and GPIO 5 to OUTPUT
 	*/
-    int counter_left = 0;
-    int counter_right = 0; 
+     
     gpio_set_direction(left,GPIO_MODE_INPUT);
     gpio_set_direction(right,GPIO_MODE_INPUT);
 	//gpio_set_direction(LED_1,GPIO_MODE_OUTPUT);
@@ -53,47 +96,10 @@ void distance_count(void *arg)
                 //vTaskDelay(100/portTICK_PERIOD_MS);
             }
         }
+
+		http_server();
 	}
 	
-}
-
-void countToDistance(counter_left,counter_right)
-{
-    double count_average,distance_final;
-    count_average = (counter_left + counter_right)/2;
-    printf("%g","Average value of counter",count_average);
-    distance_final = (count_average * 20.3)/20;
-    printf("%g","Final distance is:",distance_final);
-
-}
-
-void http_netconn_serve(struct netconn *conn) {
-    const static char http_html_hdr[] = "HTTP/1.1 200 OK\r\nContent-type: text/html\r\n\r\n";
-    const static char http_index_hml[] = "<html><head><title>Encoder distance</title></head><body></body></html>";
-    char dist[5];   // char array for stoiring distance
-    gcvt(*distance, 3, dist);
-    netconn_write(conn, http_html_hdr, sizeof(http_html_hdr)-1, NETCONN_NOCOPY);
-    netconn_write(conn, http_index_hml, sizeof(http_index_hml)-1, NETCONN_NOCOPY);
-    netconn_write(conn, dist, sizeof(dist)-1, NETCONN_NOCOPY);
-    netconn_close(conn);
-}
-
-void http_server(void *arg)
-{
-    struct netconn *conn, *newconn;
-    err_t err;
-    conn = netconn_new(NETCONN_TCP);
-    netconn_bind(conn, NULL, 80);
-    netconn_listen(conn);
-    do {
-        err = netconn_accept(conn, &newconn);   
-        if(err == ERR_OK) {
-            http_netconn_serve(newconn, &distance); //Serve distance
-            netconn_delete(newconn);
-        }
-    } while(err == ERR_OK);
-    netconn_close(conn);
-    netconn_delete(conn);
 }
 
 void app_main()
@@ -103,14 +109,14 @@ void app_main()
 	*/
 
 
-	initialize_wifi();
+	// initialize_wifi();
     
-    const esp_mqtt_client_config_t client = {
-        .uri = "postman.cloudmqtt.com",
-        .username = "ocbshoyv".
-        .password= "u7RF9Xts1g1r",
-        .port = 16557
-    };
+    // const esp_mqtt_client_config_t client = {
+    //     .uri = "postman.cloudmqtt.com",
+    //     .username = "ocbshoyv".
+    //     .password= "u7RF9Xts1g1r",
+    //     .port = 16557
+    // };
 
     xTaskCreate(&distance_count,"distance_count",4096,NULL,1,NULL);
 }
